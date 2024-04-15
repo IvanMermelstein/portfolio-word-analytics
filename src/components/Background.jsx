906c0965728cce5b6f27a39e3ab5f923c2122ff3
Iván Mermelstein <ivan.mermelstein@gmail.com>
2024-04-15 08:29:31 -0300

    first commit

---
+++ src/components/Background.jsx
@@ -0,0 +1,5 @@
+const Background = () => {
+  return <div className="bg" />;
+};
+
+export default Background;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <small>
+        Done by <a href="https://www.linkedin.com/in/ivanmermelstein/">Iván</a>
+      </small>
+    </footer>
+  );
+};
+
+export default Footer;
