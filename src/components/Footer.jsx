cd6451d3d20e33b09c5bd849b97aec44ad107ac6
Iván Mermelstein <ivan.mermelstein@gmail.com>
2024-04-16 00:07:20 -0300

    _blank on footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,10 @@ const Footer = () => {
   return (
     <footer className="footer">
       <small>
-        Done by <a href="https://www.linkedin.com/in/ivanmermelstein/">Iván</a>
+        Done by{" "}
+        <a href="https://www.linkedin.com/in/ivanmermelstein/" target="_blank">
+          Iván
+        </a>
       </small>
     </footer>
   );
